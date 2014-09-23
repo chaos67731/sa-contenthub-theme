0f25d303fc3b0eab9f287816fe65f427d07f9be3
Daniel Rhees <Daniel@Daniels-MacBook-Pro-2.local>
2014-09-23 13:10:09 -0700

    adding additonal tweaks to posts

--- extend/thumbnail.php
+++ extend/thumbnail.php
@@ -8,6 +8,7 @@ if ( function_exists( 'add_theme_support' ) ) {
 if ( function_exists( 'add_image_size' ) ) { 
 		add_image_size( 'medium-thumb', 200, 500 ); //300 pixels wide (and unlimited height)
 		add_image_size( 'large-thumb', 400, 400, true ); //(cropped)
+        add_image_size( 'post-photo-hero', 755, 557, true ); //(cropped)
 }
 
 ?>
--- single.php
+++ single.php
@@ -3,35 +3,46 @@
 	get_header(); 
 	setPostViews(get_the_ID());
 ?>
-<div class="root container"> 
+<div class="container"> 
 	<div class="Post">		
 		<?php 
 			echo get_post_meta( $post->ID, 'custom_post_heading', true );
 			echo get_post_meta( $post->ID, 'custome_post_summary', true );
 
 			if (have_posts()) :
-				while (have_posts()) : ?>
-		<header class="Post-Header">
-			<h1 class="Post-Headline"><?php the_title(); ?></h1>
-			<div class="Post-byline">
-				by <h5 class="FrontPostAuthor"><?php the_author_link(); ?> <?php echo get_the_date(); ?></h5>
-			</div>
-		</header>
+				while (have_posts()) :
+				the_post(); ?>
+
+			<header class="Post-Header">
+				<h1 class="Post-Headline"><?php the_title(); ?></h1>
+				<div class="Post-byline"><h5 class="FrontPostAuthor">By <?php the_author(); ?> <?php echo get_the_date(); ?></h5>
+				</div>
+			</header>
 		
-				<?php 
-					the_post(); 
-					the_content();  
-				?>
+			<div class="Post-Body">
+				<div class="Post-Content">
+					<?php if ( has_post_thumbnail() ) { ?>
+						<figure class="Post-PhotoHero">
+		                    <?php the_post_thumbnail('post-photo-hero'); ?>
+						</figure>
+	                <?php }; ?>
+
+					<?php 
+						the_content();
+						comments_template();
+					?>
+				</div>
+				
+				<div class="Post-SideBar">
+					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Basic Post Sidebar') ):  endif;?>
+				</div>
+			</div>
 			<?php 
 			endwhile; 
 			endif; 
 
-			comments_template();
 			?>
 
-	</div>
-	<div class="sidebar BasicPostSideBar">
-		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Other name this  Sidebar') ):  endif;?>
 	</div>
 </div>
 
