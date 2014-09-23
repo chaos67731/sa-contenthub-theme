7e7bb8991dc8683e01cd88d1083e1102f6be75a6
chaos67731 <jamesriter@Jamess-MacBook-Pro.local>
2014-09-23 13:37:42 -0700

    added post theme

---
+++ single-news.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Post Theme: NEWS
+*/
+?>
+<?php 
+	if ( !defined('ABSPATH')) exit;
+	get_header(); 
+	setPostViews(get_the_ID());
+?>
+<div class="root container"> 
+	<div class="BasicPost">
+
+		<?php 
+			echo get_post_meta( $post->ID, 'custom_post_heading', true );
+			echo get_post_meta( $post->ID, 'custome_post_summary', true );
+
+			if (have_posts()) :
+			while (have_posts()) :
+			the_post(); 
+		?>
+<h1><?php the_title(); ?> NEWS</h1>
+		<?php
+				the_content();  
+			endwhile; 
+			endif; 
+			comments_template();
+		?>
+	</div>
+
+	<div class="sidebar BasicPostSideBar">
+		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Other name this  Sidebar') ):  endif;?>
+	</div>
+</div>
+
+<?php get_footer(); ?>
